cbf4f51c1939c1f47ae10db1e73088e0a5900c09
jeff <guojianhui@shangbin.org>
2020-01-14 23:19:25 +0800

    fixed

--- Customize/WithLastDateFooter.js
+++ Customize/WithLastDateFooter.js
@@ -7,35 +7,36 @@
  *
  */
 
-import React from "react";
-import { Text, StyleSheet, AsyncStorage } from "react-native";
-import { HeaderStatus } from "../RefreshHeader";
-import {NormalFooter} from "../NormalFooter";
+import React from "react"
+import { Text, StyleSheet } from "react-native"
+import AsyncStorage from "@react-native-community/async-storage"
+import { HeaderStatus } from "../RefreshHeader"
+import { NormalFooter } from "../NormalFooter"
 
-const LoadingKey = "org.bolan9999.spring_scrollview.loading_time";
+const LoadingKey = "org.bolan9999.spring_scrollview.loading_time"
 
 export class WithLastDateFooter extends NormalFooter {
-  static style="stickyScrollView";
-  lastTime: Date;
+  static style = "stickyScrollView"
+  lastTime: Date
   constructor(props) {
-    super(props);
+    super(props)
     AsyncStorage.getItem(LoadingKey)
       .then(value => {
-        if (value) this.lastTime = new Date(value);
+        if (value) this.lastTime = new Date(value)
       })
-      .catch();
+      .catch()
   }
 
   onStateChange(oldStatus: HeaderStatus, newStatus: HeaderStatus) {
     if (newStatus === "loading") {
-      const now = new Date();
+      const now = new Date()
       AsyncStorage.setItem(LoadingKey, now.toString())
         .then(() => {
-          this.lastTime = now;
+          this.lastTime = now
         })
-        .catch();
+        .catch()
     }
-    super.onStateChange(oldStatus, newStatus);
+    super.onStateChange(oldStatus, newStatus)
   }
 
   renderContent() {
@@ -44,36 +45,36 @@ export class WithLastDateFooter extends NormalFooter {
         {this.getText()}
         {this.getTimeDescription()}
       </Text>
-    );
+    )
   }
 
   getText() {
-    return "Last Loading: ";
+    return "Last Loading: "
   }
 
   getTimeDescription() {
     if (!this.lastTime) {
-      return "never";
+      return "never"
     }
-    const now = new Date();
+    const now = new Date()
     let timeInterval = Math.floor(
       (now.getTime() - this.lastTime.getTime()) / 1000
-    );
+    )
     if (timeInterval < 60) {
-      return `${timeInterval}s ago`;
+      return `${timeInterval}s ago`
     } else if (timeInterval < 3600) {
-      return `${Math.floor(timeInterval / 60)}m ago`;
+      return `${Math.floor(timeInterval / 60)}m ago`
     } else if (
       now.getFullYear() === this.lastTime.getFullYear() &&
       now.getMonth() === this.lastTime.getMonth() &&
       now.getDate() === this.lastTime.getDate()
     ) {
-      return `${this.lastTime.getHours()}:${this.lastTime.getMinutes()}`;
+      return `${this.lastTime.getHours()}:${this.lastTime.getMinutes()}`
     } else if (now.getFullYear() === this.lastTime.getFullYear()) {
-      return `${this.lastTime.getMonth() + 1}-${this.lastTime.getDate()}`;
+      return `${this.lastTime.getMonth() + 1}-${this.lastTime.getDate()}`
     } else {
       return `${this.lastTime.getMonth() +
-      1}-${this.lastTime.getDate()}-${this.lastTime.getFullYear()}`;
+        1}-${this.lastTime.getDate()}-${this.lastTime.getFullYear()}`
     }
   }
 }
@@ -86,4 +87,4 @@ const styles = StyleSheet.create({
     textAlign: "center",
     width: 130
   }
-});
+})
